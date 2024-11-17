b483b7d4e7787a25a73c090b8864dc5732691dc7
Náthaly <nathalysantossilvadev@gmail.com>
2024-11-16 21:29:08 -0300

    finally

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  console.log(resposta);
+
+  const ativa = resposta.classList.contains("ativa");
+  resposta.classList.toggle("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+// const galeria = document.querySelectorAll(".relogio-imagens img");
+// const galeriaContainer = document.querySelector(".relogio-imagens");
+
+// function trocarImagem(event) {
+//   const img = event.currentTarget;
+//   const media = matchMedia("(min-width: 1000px)").matches;
+//   if (media) {
+//     galeriaContainer.prepend(img);
+//   }
+// }
+
+// function eventosGaleria(img) {
+//   img.addEventListener("click", trocarImagem);
+// }
+
+// galeria.forEach(eventosGaleria);
